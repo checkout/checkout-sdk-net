ae56d58818dc3b6dcba2b83c5ad2c0bb37db80d6
Ben Foster <ben.foster@checkout.com>
2018-09-27 16:57:40 +0100

    Fixed enum binding for Action Type

--- src/CheckoutSdk/Payments/ActionType.cs
+++ src/CheckoutSdk/Payments/ActionType.cs
@@ -1,12 +1,14 @@
-﻿namespace Checkout.Payments
+﻿using System.Runtime.Serialization;
+
+namespace Checkout.Payments
 {
-    
     /// <summary>
     /// Defines the type of payment action.
     /// </summary>
     public enum ActionType
     {
         Authorization,
+        [EnumMember(Value = "Card Verification")]
         CardVerification,
         Void,
         Capture,
