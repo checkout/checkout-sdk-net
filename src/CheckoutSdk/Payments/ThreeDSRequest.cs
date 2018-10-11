fecda15750b0e57e7ebd04fe66b2889b0a9747a3
Ben Foster <ben.foster@checkout.com>
2018-10-11 18:15:01 +0200

    Formatting and renamed property back to Eci

--- src/CheckoutSdk/Payments/ThreeDSRequest.cs
+++ src/CheckoutSdk/Payments/ThreeDSRequest.cs
@@ -11,22 +11,25 @@ namespace Checkout.Payments
         /// Gets or sets a value that indicates whether to process this payment as a 3D-Secure.
         /// </summary>
         public bool Enabled { get; set; }
+
         /// <summary>
         /// Gets or sets a value that indicates whether to attempt a 3D-Secure payment as non-3DS should the card issuer not be enrolled.
         /// </summary>
         [JsonProperty(PropertyName = "attempt_n3d")]
         public bool? AttemptN3D { get; set; }
+        
         /// <summary>
-        /// The Electronic Commerce Indicator security level associated with the 3D-Secure enrollment result. Required if using a third party MPI.
+        /// Gets or sets the Electronic Commerce Indicator security level associated with the 3D-Secure enrollment result. Required if using a third party MPI.
         /// </summary>
-        [JsonProperty(PropertyName = "eci")]
-        public string ElectronicCommerceIndicator { get; set; }
+        public string Eci { get; set; }
+        
         /// <summary>
-        /// Cryptographic identifier used by the card schemes to validate the cardholder authentication result (3D-Secure). Required if using an external MPI.
+        /// Gets or sets the cryptographic identifier used by the card schemes to validate the cardholder authentication result (3D-Secure). Required if using an external MPI.
         /// </summary>
         public string Cryptogram { get; set; }
+        
         /// <summary>
-        /// The 3D-Secure transaction identifier. Required if using an external MPI.
+        /// Gets or sets the 3D-Secure transaction identifier. Required if using an external MPI.
         /// </summary>
         public string Xid { get; set; }
 
