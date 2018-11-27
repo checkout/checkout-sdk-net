95aa9503f779d6364487fe8692fdabbedcbd63e0
Philippe Leonhardt <35501037+philippe-leonhardt-cko@users.noreply.github.com>
2018-11-27 13:04:27 +0100

    Support Alternative Payment Sources (#36)

---
+++ src/CheckoutSdk/Payments/AlternativePaymentSource.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+
+namespace Checkout.Payments
+{
+    /// <summary>
+    /// Represents an Alternative Payment source for a payment request.
+    /// </summary>
+    public class AlternativePaymentSource : Dictionary<string, string>, IRequestSource
+    {
+        private const string TypeField = "type";
+
+        /// <summary>
+        /// Creates a new <see cref="AlternativePaymentSource"/> instance.
+        /// </summary>
+        public AlternativePaymentSource(string type) {
+            Type = type;
+        }
+
+        /// <summary>
+        /// Gets or sets the type of source.
+        /// </summary>
+        public string Type {
+            get { return this[TypeField].ToString(); }
+            set { this[TypeField] = value; }
+        }
+    }
+}
