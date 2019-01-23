cb651255ccf2b5640e8137053c7bd8a5be483642
philippe-leonhardt-cko <35501037+philippe-leonhardt-cko@users.noreply.github.com>
2019-01-23 12:25:13 +0100

    generic implementation of SourceData and ResponseData

--- src/CheckoutSdk/Sources/ResponseData.cs
+++ src/CheckoutSdk/Sources/ResponseData.cs
@@ -1,16 +1,9 @@
-using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace Checkout.Sources
 {
     /// <summary>
     /// Defines the response data of a <see cref="SourceResponse"/>.
     /// </summary>
-    public class ResponseData
-    {
-        /// <summary>
-        /// Gets or sets the mandate reference of the response data.
-        /// </summary>
-        [JsonProperty(PropertyName = "mandate_reference")]
-        public string MandateReference { get; set; }
-    }
+    public class ResponseData : Dictionary<string, string> { }
 }
--- src/CheckoutSdk/Sources/SourceData.cs
+++ src/CheckoutSdk/Sources/SourceData.cs
@@ -1,46 +1,9 @@
-using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace Checkout.Sources
 {
     /// <summary>
     /// Defines the source data of a <see cref="SourceRequest"/>.
     /// </summary>
-    public class SourceData
-    {
-        /// <summary>
-        /// Gets or sets the first name on the source data.
-        /// </summary>
-        [JsonProperty(PropertyName = "first_name")]
-        public string FirstName { get; set; }
-
-        /// <summary>
-        /// Gets or sets the last name on the source data.
-        /// </summary>
-        [JsonProperty(PropertyName = "last_name")]
-        public string LastName { get; set; }
-
-        /// <summary>
-        /// Gets or sets the iban of the source data.
-        /// </summary>
-        [JsonProperty(PropertyName = "account_iban")]
-        public string Iban { get; set; }
-
-        /// <summary>
-        /// Gets or sets the bic on the source data.
-        /// </summary>
-        [JsonProperty(PropertyName = "bic")]
-        public string Bic { get; set; }
-
-        /// <summary>
-        /// Gets or sets the billing descriptor on the source data.
-        /// </summary>
-        [JsonProperty(PropertyName = "billing_descriptor")]
-        public string BillingDescriptor { get; set; }
-
-        /// <summary>
-        /// Gets or sets the mandate type on the source data.
-        /// </summary>
-        [JsonProperty(PropertyName = "mandate_type")]
-        public string MandateType { get; set; }
-    }
+    public class SourceData : Dictionary<string, string> { }
 }
