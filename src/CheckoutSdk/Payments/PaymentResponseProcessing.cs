1cbe3fb09a8e5559e57471dda99869abf5d41bdc
John Miliaresis <y.miliaresis@pamediakopes.gr>
2019-09-04 15:46:48 +0300

    removes: decoration

--- src/CheckoutSdk/Payments/PaymentResponseProcessing.cs
+++ src/CheckoutSdk/Payments/PaymentResponseProcessing.cs
@@ -4,10 +4,7 @@ namespace Checkout.Payments
 {
     public class PaymentResponseProcessing
     {
-        [JsonProperty(PropertyName = "retrieval_reference_number")]
         public string RetrievalReferenceNumber { get; set; }
-        
-        [JsonProperty(PropertyName = "acquirer_transaction_id")]
         public string AcquirerTransactionId { get; set; }
     }
 }
