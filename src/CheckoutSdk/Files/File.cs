e749c9c850a0ee0b9f006883e96afc72a95863f9
philippe-leonhardt-cko <35501037+philippe-leonhardt-cko@users.noreply.github.com>
2020-03-16 12:32:17 +0100

    added FilesClient; added integration tests for file upload and getting files; added test files used in integration tests

---
+++ src/CheckoutSdk/Files/File.cs
@@ -0,0 +1,35 @@
+﻿using Checkout.Common;
+
+namespace Checkout.Files
+{
+    /// <summary>
+    /// Represents a <see cref="File"/>.
+    /// </summary>
+    public class File : Resource
+    {
+        /// <summary>
+        /// Gets or sets the unique identifier of the file.
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the file.
+        /// </summary>
+        public string Filename { get; set; }
+
+        /// <summary>
+        /// Gets or sets the purpose of the file.
+        /// </summary>
+        public string Purpose { get; set; }
+
+        /// <summary>
+        /// Gets or sets the size of the file.
+        /// </summary>
+        public int Size { get; set; }
+
+        /// <summary>
+        /// Gets or sets the ISO-8601 date and time the file was uploaded on.
+        /// </summary>
+        public string UploadedOn { get; set; }
+    }
+}
