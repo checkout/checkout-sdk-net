b910b80ba19880543d98484a4f61149568ffeded
william-linden-cko <william.linden@checkout.com>
2020-04-03 18:38:28 +0200

    Introduce generic RequestSource and derive DlocalCardSource and CardSource from that.

--- src/CheckoutSdk/Payments/DlocalCardSource.cs
+++ src/CheckoutSdk/Payments/DlocalCardSource.cs
@@ -6,9 +6,9 @@ namespace Checkout.Payments
     /// <summary>
     /// Represents a full card source for a payment request (PCI compliant merchants only).
     /// </summary>
-    public class CardSource : RequestSource
+    public class DlocalCardSource : Source
     {
-        public new const string TypeName = "card";
+        public const string TypeName = "card";
 
         /// <summary>
         /// Creates a new <see cref="DlocalCardSource"/> instance with the specified card details.
@@ -16,12 +16,7 @@ namespace Checkout.Payments
         /// <param name="number">The card number.</param>
         /// <param name="expiryMonth">The two-digit expiry month of the card.</param>
         /// <param name="expiryYear">The four-digit expiry year of the card.</param>
-        public CardSource(string number, int expiryMonth, int expiryYear) : base(number, expiryMonth, expiryYear) {}
-
-        /// <summary>
-        /// Gets the type of source.
-        /// </summary>
-        public new string Type => TypeName;
+        public DlocalCardSource(string number, int expiryMonth, int expiryYear) : base(number, expiryMonth, expiryYear) {}
 
     }
 }
