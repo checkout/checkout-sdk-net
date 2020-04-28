d33c41b60c5a69f94418305563c5c9b2fdccce31
philippe-leonhardt-cko <35501037+philippe-leonhardt-cko@users.noreply.github.com>
2020-04-28 18:30:38 +0200

    add dummy test to verify 401 response on unauthorized HTTP request to ~/disputes API

---
+++ test/CheckoutSdk.Tests/Disputes/DebugDisputesEndpoint.cs
@@ -0,0 +1,21 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Shouldly;
+using Xunit;
+
+namespace Checkout.Tests.Disputes
+{
+    public class DebugDisputesEndpoint
+    {
+        [Fact]
+        public async Task Dummy()
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var response = await httpClient.GetAsync("https://api.sandbox.checkout.com/disputes");
+                response.StatusCode.ShouldBe(HttpStatusCode.Unauthorized);
+            }
+        }
+    }
+}
