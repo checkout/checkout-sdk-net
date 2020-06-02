045fbdf17b9ab8d2dbf0166f182568813b04ac7b
philippe-leonhardt-cko <35501037+philippe-leonhardt-cko@users.noreply.github.com>
2020-06-02 16:25:43 +0200

    fixed WebhooksResponse summary

--- src/CheckoutSdk/Webhooks/WebhooksResponse.cs
+++ src/CheckoutSdk/Webhooks/WebhooksResponse.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 namespace Checkout.Webhooks
 {
     /// <summary>
-    /// Defines a source response.
+    /// Defines a <see cref="WebhooksResponse"/>.
     /// </summary>
     public class WebhooksResponse : List<WebhookResponse> { }
 }
