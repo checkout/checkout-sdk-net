68c44b79abeb9ddc9a5c220cd083e777cbbeea1c
philippe-leonhardt-cko <35501037+philippe-leonhardt-cko@users.noreply.github.com>
2020-06-02 16:11:53 +0200

    defined Webhook interface and class; defined derived classes for specific webhook actions; defined WebhookRequest

---
+++ src/CheckoutSdk/Webhooks/Webhook.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+
+namespace Checkout.Webhooks
+{
+    /// <summary>
+    /// Defines a <see cref="Webhook"/>.
+    /// </summary>
+    public class Webhook : IWebhook
+    {
+        public RegistrationWebhook toRegistrationWebhook()
+        {
+            return new RegistrationWebhook(url: Url, active: Active, headers: Headers, eventTypes: EventTypes);
+        }
+
+        public UpdateWebhook toUpdateWebhook()
+        {
+            return new UpdateWebhook(url: Url, active: Active, headers: Headers, eventTypes: EventTypes);
+        }
+
+        public PartialUpdateWebhook toPartialUpdateWebhook()
+        {
+            return new PartialUpdateWebhook(url: Url, active: Active, headers: Headers, eventTypes: EventTypes);
+        }
+
+        /// <summary>
+        /// Gets or sets the webhook receiver endpoint.
+        /// </summary>
+        public string Url { get; set; }
+
+        /// <summary>
+        /// Gets or sets whether the webhook is active.
+        /// Default is true.
+        /// </summary>
+        public bool Active { get; set; } = true;
+
+        /// <summary>
+        /// Gets or sets the headers to be sent with the webhook notification.
+        /// This can be used to specify the "Authorization" Header present in webhook notifications.
+        /// If the "Authorization" Header is not specified, the API will assign a random string automatically.
+        /// </summary>
+        public Dictionary<string, string> Headers { get; set; }
+
+        /// <summary>
+        /// Gets the content type that the webhook notification will be formatted in.
+        /// </summary>
+        [Obsolete("In a previous version this could be set to 'xml', but only 'json' is supported now.")]
+        public string ContentType { get; } = "json";
+
+        /// <summary>
+        /// Gets or sets the event types for which the webhook should send notifications.
+        /// </summary>
+        public List<string> EventTypes { get; set; }
+    }
+}
