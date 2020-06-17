b15f5fd54cdbcfa4bd2d2c68f9d4273e5be02c14
philippe-leonhardt-cko <35501037+philippe-leonhardt-cko@users.noreply.github.com>
2020-06-17 10:43:06 +0200

    added idempotencyKey parameter to XML documentation

--- src/CheckoutSdk/IApiClient.cs
+++ src/CheckoutSdk/IApiClient.cs
@@ -21,7 +21,7 @@ namespace Checkout
         /// <typeparam name="TResult">The expected response type to be deserialized.</typeparam>
         /// <returns>A task that upon completion contains the specified API response data.</returns>
         Task<TResult> GetAsync<TResult>(string path, IApiCredentials credentials, CancellationToken cancellationToken);
-        
+
         /// <summary>
         /// Executes a POST request to the specified <paramref="path"/>. 
         /// </summary>
@@ -29,6 +29,7 @@ namespace Checkout
         /// <param name="credentials">The credentials used to authenticate the request.</param>
         /// <param name="cancellationToken">A cancellation token that can be used to cancel the underlying HTTP request.</param>
         /// <param name="request">Optional data that should be sent in the request body.</param>
+        /// <param name="idempotencyKey">Optional idempotency key to safely retry payment requests.</param>
         /// <typeparam name="TResult">The expected response type to be deserialized.</typeparam>
         /// <returns>A task that upon completion contains the specified API response data.</returns>
         Task<TResult> PostAsync<TResult>(string path, IApiCredentials credentials, CancellationToken cancellationToken, object request = null, string idempotencyKey = null);
@@ -40,6 +41,7 @@ namespace Checkout
         /// <param name="credentials">The credentials used to authenticate the request.</param>
         /// <param name="cancellationToken">A cancellation token that can be used to cancel the underlying HTTP request.</param>
         /// <param name="httpContent">Optional data that should be sent in the request body.</param>
+        /// <param name="idempotencyKey">Optional idempotency key to safely retry payment requests.</param>
         /// <typeparam name="TResult">The expected response type to be deserialized.</typeparam>
         /// <returns>A task that upon completion contains the specified API response data.</returns>
         Task<TResult> PostAsync<TResult>(string path, IApiCredentials credentials, CancellationToken cancellationToken, HttpContent httpContent = null, string idempotencyKey = null);
@@ -52,6 +54,7 @@ namespace Checkout
         /// <param name="resultTypeMappings">A dictionary of type mappings for different response codes.</param>
         /// <param name="cancellationToken">A cancellation token that can be used to cancel the underlying HTTP request.</param>
         /// <param name="request">Optional data that should be sent in the request body.</param>
+        /// <param name="idempotencyKey">Optional idempotency key to safely retry payment requests.</param>
         /// <returns>A task that upon completion contains the response type as determined by the <paramref="resultTypeMappings"/>.</returns>
         Task<dynamic> PostAsync(string path, IApiCredentials credentials, Dictionary<HttpStatusCode, Type> resultTypeMappings, CancellationToken cancellationToken, object request = null, string idempotencyKey = null);
 
