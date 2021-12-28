9567df99127e2a26394baef5b8ccbbd8548cf0f9
AleGladchteinParser <alejandro.gladchtein@parserdigital.com>
2021-12-28 10:56:36 -0300

    Add new field Identification to PaymentIndividualSender

--- src/CheckoutSdk/Payments/Four/Sender/Identification.cs
+++ src/CheckoutSdk/Payments/Four/Sender/Identification.cs
@@ -1,13 +1,11 @@
-﻿using Newtonsoft.Json;
-
-namespace Checkout.Payments.Four.Sender
+﻿namespace Checkout.Payments.Four.Sender
 {
     public class Identification
     {
-        public IdentificationType Type { get; set; }
+        public IdentificationType? Type { get; set; }
+
         public string Number { get; set; }
 
-        [JsonProperty("issuing_country")]
         public string IssuingCountry { get; set; }
     }
 }
