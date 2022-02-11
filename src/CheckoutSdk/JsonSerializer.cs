1d480f82046179b11dca920c3fd87ee041f2d6b6
CI / CD Pipeline <ci@coverwise.com>
2022-02-11 15:13:37 +0000

    Tidied the csproj and merged the JsonSerializer classes

--- src/CheckoutSdk/JsonSerializer.cs
+++ src/CheckoutSdk/JsonSerializer.cs
@@ -1,19 +1,73 @@
-#if NETSTANDARD2_0
-
 using Checkout.Instruments.Four.Get.Util;
 using Checkout.Instruments.Four.Update.Util;
 using Checkout.Workflows.Four.Actions.Response.Util;
 using Checkout.Workflows.Four.Conditions.Response.Util;
+using System;
+using System.Collections.Generic;
+
+#if NET5_0_OR_GREATER
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+#else 
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 using Newtonsoft.Json.Serialization;
-using System;
-using System.Collections.Generic;
+#endif
 
 namespace Checkout
 {
     public class JsonSerializer : ISerializer
     {
+#if NET5_0_OR_GREATER
+
+        private readonly JsonSerializerOptions _serializerSettings;
+
+        public JsonSerializer(Action<JsonSerializerOptions> configureSettings = null)
+        {
+            _serializerSettings = CreateSerializerSettings(configureSettings);
+        }
+
+
+        public object Deserialize(string payload, Type objectType)
+        {
+            CheckoutUtils.ValidateParams("payload", payload);
+            return System.Text.Json.JsonSerializer.Deserialize(payload, objectType, _serializerSettings);
+        }
+
+        public IDictionary<string, object> Deserialize(string payload)
+        {
+            return System.Text.Json.JsonSerializer.Deserialize<Dictionary<string, object>>(payload, _serializerSettings);
+        }
+
+        public string Serialize(object payload)
+        {
+            CheckoutUtils.ValidateParams("payload", payload);
+            return System.Text.Json.JsonSerializer.Serialize(payload, payload.GetType(), _serializerSettings);
+        }
+
+        private static JsonSerializerOptions CreateSerializerSettings(Action<JsonSerializerOptions> configureSettings)
+        {
+            var settings = new JsonSerializerOptions(JsonSerializerDefaults.Web)
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault
+            };
+            settings.Converters.Add(new JsonStringEnumConverter(JsonNamingPolicy.CamelCase, true));
+
+            settings.Converters.Add(new GetInstrumentResponseConverter());
+            settings.Converters.Add(new UpdateInstrumentResponseConverter());
+            settings.Converters.Add(new WorkflowActionResponseConverter());
+            settings.Converters.Add(new WorkflowActionResponseConverter());
+
+
+            configureSettings?.Invoke(settings);
+
+            return settings;
+        }
+
+
+#else
         private readonly JsonSerializerSettings _serializerSettings;
 
         public JsonSerializer(Action<JsonSerializerSettings> configureSettings = null)
@@ -68,7 +122,7 @@ namespace Checkout
             };
 
             return converter;
-        }
+        }    
+#endif
     }
 }
-#endif
