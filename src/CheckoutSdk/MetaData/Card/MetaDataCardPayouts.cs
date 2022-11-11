f00c43aca149d08e111f6d8f57228c36f7949957
Armando Lena <armando.lena@parserdigital.com>
2022-11-11 12:10:00 +0100

    Add new car metadata class objects

---
+++ src/CheckoutSdk/MetaData/Card/MetaDataCardPayouts.cs
@@ -0,0 +1,18 @@
+﻿namespace Checkout.MetaData.Card
+{
+    public class MetaDataCardPayouts
+    {
+        public CardPayoutsTransferType? DomesticNonMoneyTransfer { get; set; }
+
+        public CardPayoutsTransferType? CrossBorderNonMoneyTransfer { get; set; }
+        
+        public CardPayoutsTransferType? DomesticGambling { get; set; }
+        
+        public CardPayoutsTransferType? CrossBorderGambling { get; set; }
+        
+        public CardPayoutsTransferType? DomesticMoneyTransfer { get; set; }
+        
+        public CardPayoutsTransferType? CrossBorderMoneyTransfer { get; set; }
+        
+    }
+}
---
+++ src/CheckoutSdk/MetaData/Card/MetaDataCardResponse.cs
@@ -0,0 +1,30 @@
+﻿using Checkout.Common;
+
+namespace Checkout.MetaData.Card
+{
+    public class MetaDataCardResponse : HttpMetadata
+    {
+        public string Bin { get; set; }
+
+        public string Scheme { get; set; }
+
+        public SchemeLocalType? SchemeLocal { get; set; }
+        
+        public CardType? CardType { get; set; }
+        
+        public CardCategory? CardCategory { get; set; }
+        
+        public string Issuer { get; set; }
+        
+        public CountryCode? IssuerCountry { get; set; }
+        
+        public string IssuerCountryName { get; set; }
+        
+        public string ProductId { get; set; }
+        
+        public string ProductType { get; set; }
+        
+        public MetaDataCardPayouts CardPayouts { get; set; }
+        
+    }
+}
