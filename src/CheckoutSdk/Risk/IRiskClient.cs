99717b041b6c41d7edbdd6dc54f4c7a2ddb900c3
Armando Ibarra <aibarrakerzon@gmail.com>
2022-12-19 21:10:04 -0600

    Deprecated Risk client (CS1/CS2)

--- src/CheckoutSdk/Risk/IRiskClient.cs
+++ src/CheckoutSdk/Risk/IRiskClient.cs
@@ -1,10 +1,12 @@
 using Checkout.Risk.PreAuthentication;
 using Checkout.Risk.PreCapture;
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace Checkout.Risk
 {
+    [Obsolete("Risk endpoints are no longer supported officially, This module will be removed in a future release.", false)]
     public interface IRiskClient
     {
         Task<PreAuthenticationAssessmentResponse> RequestPreAuthenticationRiskScan(
