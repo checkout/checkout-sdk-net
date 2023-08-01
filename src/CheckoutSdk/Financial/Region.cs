944ea8f1f95e4c0a228a0cf856b45d078eccdf4e
Armando Rodríguez <127134616+armando-rodriguez-cko@users.noreply.github.com>
2023-08-01 12:35:29 +0200

    Adds Intra values for Region enum and NotSet in CardCategory enum

--- src/CheckoutSdk/Financial/Region.cs
+++ src/CheckoutSdk/Financial/Region.cs
@@ -5,7 +5,10 @@ namespace Checkout.Financial
     public enum Region
     {
         [EnumMember(Value = "Domestic")] Domestic,
+        [EnumMember(Value = "Intra")] Intra,
+        [EnumMember(Value = "IntraEEA")] IntraEea,
+        [EnumMember(Value = "IntraEuropean_SEPA")] IntraEuropeanSepa,
+        [EnumMember(Value = "International")] International,
         [EnumMember(Value = "EEA")] Eea,
-        [EnumMember(Value = "International")] International
     }
 }
