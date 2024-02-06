113610b9ab617b5bfc07fba565c0edd3e81cdcf4
Armando Rodríguez <127134616+armando-rodriguez-cko@users.noreply.github.com>
2024-02-06 14:38:30 +0100

    Update Payment Schema Properties (#368)
    - Update `Payment Contexts` - `Payment Request`
    - Update `Giropay` Schema
    - Update missing properties and tests

---
+++ src/CheckoutSdk/Payments/Request/PaymentSegment.cs
@@ -0,0 +1,11 @@
+namespace Checkout.Payments.Request
+{
+    public class PaymentSegment
+    {
+        public string Brand { get; set; }
+
+        public string BusinessCategory { get; set; }
+
+        public string Market { get; set; }
+    }
+}
