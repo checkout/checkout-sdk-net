fdd1994fd9e040859c5f2d1cfb392b169c9792fb
Armando Rodríguez <127134616+armando-rodriguez-cko@users.noreply.github.com>
2024-04-04 14:03:34 +0200

    Update purpose types

--- src/CheckoutSdk/Accounts/AccountsFilePurpose.cs
+++ src/CheckoutSdk/Accounts/AccountsFilePurpose.cs
@@ -12,9 +12,13 @@
         public static AccountsFilePurpose BankVerification => new AccountsFilePurpose("bank_verification");
 
         public static AccountsFilePurpose Identification => new AccountsFilePurpose("identification");
+        
+        public static AccountsFilePurpose IdentityVerification => new AccountsFilePurpose("identity_verification");
 
         public static AccountsFilePurpose CompanyVerification => new AccountsFilePurpose("company_verification");
 
         public static AccountsFilePurpose FinancialVerification => new AccountsFilePurpose("financial_verification");
+        
+        public static AccountsFilePurpose TaxVerification => new AccountsFilePurpose("tax_verification");
     }
 }
