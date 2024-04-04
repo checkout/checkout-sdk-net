756b79007464d5f2f89e95aa10a80a3da708a9dc
Armando Rodríguez <127134616+armando-rodriguez-cko@users.noreply.github.com>
2024-04-04 14:03:08 +0200

    Update card metadata response properties

--- src/CheckoutSdk/Common/SchemeLocalType.cs
+++ src/CheckoutSdk/Common/SchemeLocalType.cs
@@ -4,8 +4,13 @@ namespace Checkout.Common
 {
     public enum SchemeLocalType
     {
+        [EnumMember(Value = "accel")] Accel,
         [EnumMember(Value = "cartes_bancaires")] CartesBancaires,
         [EnumMember(Value = "mada")] Mada,
+        [EnumMember(Value = "nyce")] Nyce,
         [EnumMember(Value = "omannet")] Omannet,
+        [EnumMember(Value = "pulse")] Pulse,
+        [EnumMember(Value = "star")] Star,
+        [EnumMember(Value = "upi")] Upi,
     }
 }
