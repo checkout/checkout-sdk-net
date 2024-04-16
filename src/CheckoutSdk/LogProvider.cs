44b7b90ea9fa756dc0939ce5a3b4ecfaddc7b936
leandro-tylkovitch-cko <leandro.tylkovitch@checkout.com>
2024-04-16 11:48:26 +0200

    Change logprovider

--- src/CheckoutSdk/LogProvider.cs
+++ src/CheckoutSdk/LogProvider.cs
@@ -8,25 +8,17 @@ namespace Checkout
 {
     public static class LogProvider
     {
-        private static IDictionary<string, ILogger> _loggers = new Dictionary<string, ILogger>();
         private static ILoggerFactory _loggerFactory = new LoggerFactory();
 
         public static void SetLogFactory(ILoggerFactory factory)
         {
             _loggerFactory?.Dispose();
             _loggerFactory = factory;
-            _loggers.Clear();
         }
 
-        public static ILogger GetLogger(Type clazz)
+        public static ILogger GetLogger(Type loggerType)
         {
-            var category = clazz.FullName;
-            if (category != null && !_loggers.ContainsKey(category))
-            {
-                _loggers[category] = _loggerFactory?.CreateLogger(category) ?? NullLogger.Instance;
-            }
-
-            return category is null ? NullLogger.Instance : _loggers[category];
+            return loggerType is null ? NullLogger.Instance : _loggerFactory.CreateLogger(loggerType);
         }
     }
 }
