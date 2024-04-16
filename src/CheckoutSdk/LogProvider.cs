68749c923c3cd8243c3517c0226f5477667e396b
leandro-tylkovitch-cko <leandro.tylkovitch@checkout.com>
2024-04-16 15:45:55 +0200

    Identation change

--- src/CheckoutSdk/LogProvider.cs
+++ src/CheckoutSdk/LogProvider.cs
@@ -5,20 +5,18 @@ using System;
 
 namespace Checkout
 {
-  public static class LogProvider
-{
+  public static class LogProvider 
+  {
     private static ILoggerFactory _loggerFactory = new LoggerFactory();
-
     public static void SetLogFactory(ILoggerFactory factory)
     {
         _loggerFactory?.Dispose();
         _loggerFactory = factory;
     }
-
     public static ILogger GetLogger(Type loggerType)
     {
         return  _loggerFactory.CreateLogger(loggerType);
     }
-}
+  }
 }
 #endif
