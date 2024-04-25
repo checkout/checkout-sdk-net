d6d5d65e365f8ed275d4e02c9a9970af922592bb
Armando Rodríguez <127134616+armando-rodriguez-cko@users.noreply.github.com>
2024-04-25 19:38:29 +0200

    Log provider improvements (#375)

--- src/CheckoutSdk/LogProvider.cs
+++ src/CheckoutSdk/LogProvider.cs
@@ -2,31 +2,23 @@
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
 using System;
-using System.Collections.Generic;
 
 namespace Checkout
 {
+   
     public static class LogProvider
     {
-        private static IDictionary<string, ILogger> _loggers = new Dictionary<string, ILogger>();
         private static ILoggerFactory _loggerFactory = new LoggerFactory();
 
         public static void SetLogFactory(ILoggerFactory factory)
         {
             _loggerFactory?.Dispose();
-            _loggerFactory = factory;
-            _loggers.Clear();
+            _loggerFactory = factory ?? new LoggerFactory();
         }
 
-        public static ILogger GetLogger(Type clazz)
+        public static ILogger GetLogger(Type loggerType)
         {
-            var category = clazz.FullName;
-            if (category != null && !_loggers.ContainsKey(category))
-            {
-                _loggers[category] = _loggerFactory?.CreateLogger(category) ?? NullLogger.Instance;
-            }
-
-            return category is null ? NullLogger.Instance : _loggers[category];
+            return loggerType is null ? NullLogger.Instance : _loggerFactory.CreateLogger(loggerType) ?? NullLogger.Instance;;
         }
     }
 }
