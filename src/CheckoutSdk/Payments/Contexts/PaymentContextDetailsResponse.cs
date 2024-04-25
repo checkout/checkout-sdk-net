8c13151f873d0eef69374d7336355232b7ae5127
Armando Rodríguez <127134616+armando-rodriguez-cko@users.noreply.github.com>
2024-04-25 12:01:02 +0200

    Add Klarna Payment Context Support (#388)

--- src/CheckoutSdk/Payments/Contexts/PaymentContextDetailsResponse.cs
+++ src/CheckoutSdk/Payments/Contexts/PaymentContextDetailsResponse.cs
@@ -4,6 +4,8 @@ namespace Checkout.Payments.Contexts
 {
     public class PaymentContextDetailsResponse : HttpMetadata
     {
+        public PaymentContextDetailsStatusType? Status { get; set; }
+        
         public PaymentContextsResponse PaymentRequest { get; set; }
 
         public PaymentContextsPartnerMetadata PartnerMetadata { get; set; }
