935cb80b5447cf5ec10bc3ae52ac0c75c4a77a3a
leandro-tylkovitch-cko <leandro.tylkovitch@checkout.com>
2024-05-17 13:16:30 +0200

    Added new object Destination

---
+++ src/CheckoutSdk/Common/Destination.cs
@@ -0,0 +1,25 @@
+﻿namespace Checkout.Common
+{
+    public class Destination
+    {
+        public AccountType AccountType { get; set; }
+        
+        public string AccountNumber { get; set; }
+        
+        public string BankCode { get; set; }
+        
+        public string BranchCode { get; set; }
+        
+        public string Iban { get; set; }
+        
+        public string Bban { get; set; }
+        
+        public string SwiftBic { get; set; }
+        
+        public CountryCode Country { get; set; }
+        
+        public AccountHolder AccountHolder { get; set; }
+        
+        public BankDetails Bank  { get; set; }
+    }
+}
