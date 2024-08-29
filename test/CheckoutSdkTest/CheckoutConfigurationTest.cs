c34be1512b57234f5101c42898c95c0aac5956d0
Armando Rodríguez <127134616+armando-rodriguez-cko@users.noreply.github.com>
2024-08-29 10:12:11 +0200

    Update DNS Merchant regular expression length (#414)

--- test/CheckoutSdkTest/CheckoutConfigurationTest.cs
+++ test/CheckoutSdkTest/CheckoutConfigurationTest.cs
@@ -19,9 +19,10 @@ namespace Checkout
         }
 
         [Theory]
-        [InlineData("123dmain", "https://123dmain.api.sandbox.checkout.com/")]
-        [InlineData("123domain", "https://123domain.api.sandbox.checkout.com/")]
-        [InlineData("1234domain", "https://1234domain.api.sandbox.checkout.com/")]
+        [InlineData("a", "https://a.api.sandbox.checkout.com/")]
+        [InlineData("ab", "https://ab.api.sandbox.checkout.com/")]
+        [InlineData("abc", "https://abc.api.sandbox.checkout.com/")]
+        [InlineData("abc1", "https://abc1.api.sandbox.checkout.com/")]
         [InlineData("12345domain", "https://12345domain.api.sandbox.checkout.com/")]
         public void ShouldCreateConfigurationWithSubdomain(string subdomain, string expectedUri)
         {
@@ -38,9 +39,11 @@ namespace Checkout
 
         [Theory]
         [InlineData("", "https://api.sandbox.checkout.com/")]
-        [InlineData("123", "https://api.sandbox.checkout.com/")]
-        [InlineData("123bad", "https://api.sandbox.checkout.com/")]
-        [InlineData("12345domainBad", "https://api.sandbox.checkout.com/")]
+        [InlineData(" ", "https://api.sandbox.checkout.com/")]
+        [InlineData("  ", "https://api.sandbox.checkout.com/")]
+        [InlineData(" - ", "https://api.sandbox.checkout.com/")]
+        [InlineData("a b", "https://api.sandbox.checkout.com/")]
+        [InlineData("ab c1", "https://api.sandbox.checkout.com/")]
         public void ShouldCreateConfigurationWithBadSubdomain(string subdomain, string expectedUri)
         {
             var credentials = new StaticKeysSdkCredentials(ValidDefaultSk, ValidDefaultPk);
