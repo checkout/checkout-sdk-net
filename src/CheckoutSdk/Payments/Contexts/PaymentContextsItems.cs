ab390db30562c49a3c0010adcceafef28483b216
Armando Rodríguez <armando.rodriguez@checkout.com>
2024-11-11 12:03:32 +0100

    Fix int nullables

--- src/CheckoutSdk/Payments/Contexts/PaymentContextsItems.cs
+++ src/CheckoutSdk/Payments/Contexts/PaymentContextsItems.cs
@@ -5,17 +5,17 @@ namespace Checkout.Payments.Contexts
         public PaymentContextItemType? Type { get; set; }
         public string Name { get; set; }
 
-        public int Quantity { get; set; }
+        public int? Quantity { get; set; }
 
-        public int UnitPrice { get; set; }
+        public int? UnitPrice { get; set; }
 
         public string Reference { get; set; }
 
-        public int TotalAmount { get; set; }
+        public int? TotalAmount { get; set; }
 
-        public int TaxAmount { get; set; }
+        public int? TaxAmount { get; set; }
 
-        public int DiscountAmount { get; set; }
+        public int? DiscountAmount { get; set; }
 
         public string Url { get; set; }
 
