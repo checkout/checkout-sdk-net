0bf90336a1dd24b527d8e118a355ac29cd229d49
Armando Rodríguez <127134616+armando-rodriguez-cko@users.noreply.github.com>
2024-12-16 18:27:02 +0100

    Release 4.5.0
    - Updates Payment Request Authentication
    - Refactoring and Enhancing Account Entity Management

--- src/CheckoutSdk/Payments/PaymentStatus.cs
+++ src/CheckoutSdk/Payments/PaymentStatus.cs
@@ -4,25 +4,49 @@ namespace Checkout.Payments
 {
     public enum PaymentStatus
     {
-        [EnumMember(Value = "Active")] Active,
-        [EnumMember(Value = "Pending")] Pending,
-        [EnumMember(Value = "Authorized")] Authorized,
-        [EnumMember(Value = "Card Verified")] CardVerified,
-        [EnumMember(Value = "Voided")] Voided,
+        [EnumMember(Value = "Active")]
+        Active,
+        
+        [EnumMember(Value = "Pending")]
+        Pending,
+        
+        [EnumMember(Value = "Authorized")]
+        Authorized,
+        
+        [EnumMember(Value = "Card Verified")]
+        CardVerified,
+        
+        [EnumMember(Value = "Voided")]
+        Voided,
 
         [EnumMember(Value = "Partially Captured")]
         PartiallyCaptured,
 
-        [EnumMember(Value = "Captured")] Captured,
+        [EnumMember(Value = "Captured")]
+        Captured,
 
         [EnumMember(Value = "Partially Refunded")]
         PartiallyRefunded,
 
-        [EnumMember(Value = "Refunded")] Refunded,
-        [EnumMember(Value = "Declined")] Declined,
-        [EnumMember(Value = "Canceled")] Canceled,
-        [EnumMember(Value = "Expired")] Expired,
-        [EnumMember(Value = "Requested")] Requested,
-        [EnumMember(Value = "Paid")] Paid
+        [EnumMember(Value = "Refunded")]
+        Refunded,
+        
+        [EnumMember(Value = "Declined")]
+        Declined,
+        
+        [EnumMember(Value = "Canceled")]
+        Canceled,
+        
+        [EnumMember(Value = "Expired")]
+        Expired,
+        
+        [EnumMember(Value = "Requested")]
+        Requested,
+        
+        [EnumMember(Value = "Paid")]
+        Paid,
+        
+        [EnumMember(Value = "Returned")]
+        Returned,
     }
 }
