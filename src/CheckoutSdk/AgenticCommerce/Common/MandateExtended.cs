27c3cdf211316726f405fbdb5c27a1bc9bec63ef
david ruiz <david.ruiz@checkout.com>
2025-11-21 19:43:55 +0100

    MandateExtended does not need inherited members

--- src/CheckoutSdk/AgenticCommerce/Common/MandateExtended.cs
+++ src/CheckoutSdk/AgenticCommerce/Common/MandateExtended.cs
@@ -12,24 +12,5 @@ namespace Checkout.AgenticCommerce.Common
         /// [Required]
         /// </summary>
         public string Id { get; set; }
-
-        /// <summary>
-        /// Purchase threshold configuration
-        /// </summary>
-        public PurchaseThreshold PurchaseThreshold { get; set; }
-
-        /// <summary>
-        /// A brief description of the purchase intent
-        /// &lt;= 255 characters
-        /// [Required]
-        /// </summary>
-        public string Description { get; set; }
-
-        /// <summary>
-        /// The date and time when the purchase intent expires, in ISO 8601 format.
-        /// This value must be set to a date and time in the future
-        /// [Required]
-        /// </summary>
-        public DateTime? ExpirationDate { get; set; }
     }
 }
