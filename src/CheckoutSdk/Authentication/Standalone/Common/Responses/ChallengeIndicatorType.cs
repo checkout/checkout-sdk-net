f807c927d209c90311585ea6bd432a8350a424ff
david ruiz <david.ruiz@checkout.com>
2025-12-09 16:07:36 +0100

    Update challenge indicator type to required in payment session and submission responses

--- src/CheckoutSdk/Authentication/Standalone/Common/Responses/ChallengeIndicatorType.cs
+++ src/CheckoutSdk/Authentication/Standalone/Common/Responses/ChallengeIndicatorType.cs
@@ -15,5 +15,20 @@ namespace Checkout.Authentication.Standalone.Common.Responses
 
         [EnumMember(Value = "challenge_requested_mandate")]
         ChallengeRequestedMandate,
+
+        [EnumMember(Value = "low_value")]
+        LowValue,
+
+        [EnumMember(Value = "trusted_listing")]
+        TrustedListing,
+
+        [EnumMember(Value = "trusted_listing_prompt")]
+        TrustedListingPrompt,
+
+        [EnumMember(Value = "transaction_risk_assessment")]
+        TransactionRiskAssessment,
+
+        [EnumMember(Value = "data_share")]
+        DataShare
     }
 }
