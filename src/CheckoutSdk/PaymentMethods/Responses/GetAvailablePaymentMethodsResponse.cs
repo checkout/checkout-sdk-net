7de240f0beacbd21951bfcd10b14b2460cf8a682
david ruiz <david.ruiz@checkout.com>
2026-02-05 16:33:14 +0100

    Review + unit and integration tests of: ApplePayClient, PaymentMethodsClient, StandaloneAccountUpdaterClient

--- src/CheckoutSdk/PaymentMethods/Responses/GetAvailablePaymentMethodsResponse.cs
+++ src/CheckoutSdk/PaymentMethods/Responses/GetAvailablePaymentMethodsResponse.cs
@@ -1,6 +1,9 @@
 using System.Collections.Generic;
 using Checkout.Common;
 
+using Checkout.PaymentMethods.Entities;
+using Checkout.Common;
+
 namespace Checkout.PaymentMethods.Responses
 {
     public class GetAvailablePaymentMethodsResponse : Resource
@@ -9,6 +12,7 @@ namespace Checkout.PaymentMethods.Responses
         /// The enabled payment methods for the processing channel
         /// [Required]
         /// </summary>
+        /// [Required]
         public IList<PaymentMethod> Methods { get; set; }
     }
 }
