b83ee57e42b19e757d8c66e20391d3683f226b32
david ruiz <david.ruiz@checkout.com>
2026-02-05 11:41:48 +0100

    Fine tune of the last clients

--- src/CheckoutSdk/ApplePay/Requests/GenerateSigningRequestRequest.cs
+++ src/CheckoutSdk/ApplePay/Requests/GenerateSigningRequestRequest.cs
@@ -1,3 +1,5 @@
+using Checkout.ApplePay.Entities;
+
 namespace Checkout.ApplePay.Requests
 {
     public class GenerateSigningRequestRequest
@@ -6,6 +8,6 @@ namespace Checkout.ApplePay.Requests
         /// The protocol version of the encryption type used.
         /// Default: "ec_v1"
         /// </summary>
-        public string ProtocolVersion { get; set; } = "ec_v1";
+        public ProtocolVersions ProtocolVersion { get; set; } = ProtocolVersions.EcV1;
     }
 }
