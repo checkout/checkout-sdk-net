499134d5f6ba23af4f26241b7067e1fd23807290
david ruiz <david.ruiz@checkout.com>
2026-02-09 15:41:51 +0100

    FaceAuthentication check

---
+++ src/CheckoutSdk/Identities/Entities/FaceDetails.cs
@@ -0,0 +1,13 @@
+namespace Checkout.Identities.Entities
+{
+    public class FaceDetails
+    {
+        /// <summary>
+        /// The URL to the face image
+        /// [Required]
+        /// </summary>
+        public string ImageSignedUrl { get; set; }
+    }
+}
+
+
--- src/CheckoutSdk/Identities/FaceAuthentication/Requests/FaceAuthenticationRequest.cs
+++ src/CheckoutSdk/Identities/FaceAuthentication/Requests/FaceAuthenticationRequest.cs
@@ -1,30 +1,17 @@
-using Checkout.Identities.FaceAuthentication.Responses;
-
 namespace Checkout.Identities.FaceAuthentication.Requests
 {
     public class FaceAuthenticationRequest
     {
         /// <summary>
-        /// The applicant's unique identifier (Required)
+        /// The applicant's unique identifier
+        /// [Required]
         /// </summary>
         public string ApplicantId { get; set; }
 
         /// <summary>
-        /// Your configuration ID (Required)
+        /// Your configuration ID
+        /// [Required]
         /// </summary>
         public string UserJourneyId { get; set; }
-    }
-
-    public class FaceAuthenticationAttemptRequest
-    {
-        /// <summary>
-        /// The URL to redirect the applicant to after the attempt (Required)
-        /// </summary>
-        public string RedirectUrl { get; set; }
-
-        /// <summary>
-        /// The applicant's details (Optional)
-        /// </summary>
-        public ClientInformation ClientInformation { get; set; }
     }
 }
