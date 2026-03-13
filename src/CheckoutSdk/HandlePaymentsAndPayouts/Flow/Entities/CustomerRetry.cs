3038469d3e40a5dfc893cb74fac5e2c7eaf0349d
david ruiz <david.ruiz@checkout.com>
2026-03-13 11:04:52 +0100

    Removed more unused usings

--- src/CheckoutSdk/HandlePaymentsAndPayouts/Flow/Entities/CustomerRetry.cs
+++ src/CheckoutSdk/HandlePaymentsAndPayouts/Flow/Entities/CustomerRetry.cs
@@ -1,10 +1,3 @@
-using Checkout.Common;
-using Checkout.HandlePaymentsAndPayouts.Common;
-using Newtonsoft.Json;
-
-using System.Collections.Generic;
-using System.Runtime.Serialization;
-
 namespace Checkout.HandlePaymentsAndPayouts.Flow.Entities
 {
     public class CustomerRetry
--- src/CheckoutSdk/HandlePaymentsAndPayouts/Flow/Entities/PaymentMethodConfigurationBase.cs
+++ src/CheckoutSdk/HandlePaymentsAndPayouts/Flow/Entities/PaymentMethodConfigurationBase.cs
@@ -1,9 +1,5 @@
 using Checkout.Common;
 using Checkout.HandlePaymentsAndPayouts.Common;
-using Newtonsoft.Json;
-
-using System.Collections.Generic;
-using System.Runtime.Serialization;
 
 namespace Checkout.HandlePaymentsAndPayouts.Flow.Entities
 {
