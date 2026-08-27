5a6aa70cd4401fe2d154261dbb86bf6e44f13df3
Armando Rodríguez <127134616+armando-rodriguez-cko@users.noreply.github.com>
2026-08-27 12:34:22 +0200

    feat: add optional amount to VoidRequest for partial voids (INT-1689) (#597)

--- src/CheckoutSdk/Payments/VoidRequest.cs
+++ src/CheckoutSdk/Payments/VoidRequest.cs
@@ -4,6 +4,11 @@ namespace Checkout.Payments
 {
     public class VoidRequest
     {
+        /// <summary>
+        /// The amount to void, min 0, max 9999999999. If not specified, the full payment amount is voided.
+        /// </summary>
+        public long? Amount { get; set; }
+
         public string Reference { get; set; }
 
         public IDictionary<string, object> Metadata { get; set; } = new Dictionary<string, object>();
